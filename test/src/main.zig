4d5cee5fd1bcb16af685424d40eb96f76f59ee49
wordandahalf <10679776+wordandahalf@users.noreply.github.com>
2025-09-07 15:00:44 -0400

    change: simplify PIO program by using side-set more extensively, properly pack register values into FIFO

--- test/src/main.zig
+++ test/src/main.zig
@@ -7,37 +7,59 @@ const pio = rp2xxx.pio;
 const config = @import("config.zig");
 const m64282fp = @import("m64282fp.zig");
 const program = @import("pio/m64282fp.zig").program;
+const packRegisters = @import("utils.zig").packRegisters;
 
 pub const microzig_options: microzig.Options = .{
-    .interrupts = .{ .PIO0_IRQ_0 = .{ .c = pio_ready_for_data } }
+    .interrupts = .{
+        .PIO0_IRQ_0 = .{ .c = pio_ready_for_data },
+        .PIO0_IRQ_1 = .{ .c = adc_sample_pixel },
+    }
 };
 
 const pio0 = pio.num(0);
 const sm = pio.StateMachine.sm0;
 var device = m64282fp.init();
 
+var pixel_buffer: [m64282fp.sensor_resolution]u12 = undefined;
+var pixel_buffer_ptr: u32 = 0;
+
 fn pio_ready_for_data() callconv(.c) void {
     const cs = microzig.interrupt.enter_critical_section();
     defer cs.leave();
 
-    pio0.sm_blocking_write(sm,  @as(u32, device.regs().exposure()) * config.m64282fp_clock_periods_per_exposure - 2);
-    pio0.sm_blocking_write(sm, m64282fp.sensor_resolution - 2);
+    pixel_buffer_ptr = 0;
+    pio0.sm_blocking_write(sm, m64282fp.sensor_resolution);
+    pio0.sm_blocking_write(sm,  @as(u32, device.regs().exposure()) * config.m64282fp_clock_periods_per_exposure);
 
     pio0.sm_clear_interrupt(sm, .irq0, .statemachine);
 }
 
+fn adc_sample_pixel() callconv(.c) void {
+    const cs = microzig.interrupt.enter_critical_section();
+    defer cs.leave();
+
+    pixel_buffer[pixel_buffer_ptr] = rp2xxx.adc.convert_one_shot_blocking(.ain0) catch blk: {
+        break :blk 0;
+    };
+    pixel_buffer_ptr += 1;
+
+    pio0.sm_clear_interrupt(sm, .irq1, .statemachine);
+}
+
 pub fn main() !void {
     microzig.cpu.interrupt.enable(.PIO0_IRQ_0);
+    microzig.cpu.interrupt.enable(.PIO0_IRQ_1);
 
     for (0..6) |it| { pio0.gpio_init(gpio.num(@truncate(0 + it))); }
     pio0.sm_set_pindir(sm, 0, 6, .out);
+    pio0.sm_set_pin(sm, 0, 6, 0);
 
     const options: pio.LoadAndStartProgramOptions = .{
         .clkdiv = pio.ClkDivOptions.from_float(config.m64282fp_pio_clock_divider),
         .pin_mappings = .{
-            .set = .{ .base = 0, .count = 4, },
-            .out = .{ .base = 4, .count = 1, },
-            .side_set = .{ .base = 5, .count = 1 },
+            .set = .{ .base = 0, .count = 2 },
+            .side_set = .{ .base = 2, .count = 3 },
+            .out = .{ .base = 5, .count = 1 }
         },
         .shift = .{
             .out_shiftdir = .left,
@@ -50,12 +72,11 @@ pub fn main() !void {
     pio0.sm_enable_interrupt(sm, .irq0, .statemachine);
     pio0.sm_load_and_start_program(sm, program, options) catch unreachable;
 
-    // For my sanity, we waste 21 bits per FIFO word when encoding register values.
-    // This is not ideal, but meticulously constructing the three FIFO words with the
-    // 8 packed 11-bit address-value pairs is a pain in the ass since it is isn't byte-aligned.
-    // Instead, we just use 11 bits per word and then discard the rest.
-    for (0..8, device.registers.values) |i, it| {
-        pio0.sm_blocking_write(sm, @as(u32, @as(u11, @truncate(i)) << 8 | it) << 21);
+    var buffer: [3]u32 = undefined;
+    packRegisters(device.registers.values, buffer[0..]);
+
+    for (buffer) |it| {
+        pio0.sm_blocking_write(sm, it);
     }
 
     pio0.sm_set_enabled(sm, true);
--- test/src/utils.zig
+++ test/src/utils.zig
@@ -3,3 +3,20 @@ const std = @import("std");
 pub fn compileErrorFmt(comptime fmt: []const u8, args: anytype) noreturn {
     @compileError(std.fmt.comptimePrint(fmt, args));
 }
+
+pub fn packRegisters(input: [8]u8, output: []u32) void {
+    var packed_value: u96 = 0;
+
+    for (input, 0..) |it, i| {
+        const addr: u96 = i & 0b111;
+        const val:  u96 = it;
+        const reg_bits = (addr << 8) | val;
+
+        const shift: u7 = @truncate(85 - 11*i);
+        packed_value |= reg_bits << shift;
+    }
+
+    output[0] = @truncate(packed_value >> 64);
+    output[1] = @truncate(packed_value >> 32);
+    output[2] = @truncate(packed_value >> 0);
+}
